531e99dc172f9e45b23b99b5ddf6ca1b154dcff4
fiza155 <141968587+fiza155@users.noreply.github.com>
2024-10-21 13:35:19 +0500

    Update CategoryCard.jsx

--- src/grocerySite/components/ExploreCategory/CategoryCard.jsx
+++ src/grocerySite/components/ExploreCategory/CategoryCard.jsx
@@ -21,29 +21,3 @@ function CategoryCard({ name, items, image, bgColor }) {
 
 export default CategoryCard;
 
-// import React from "react";
-
-// function CategoryCard({ name, items, image, bgColor }) {
-//   return (
-//     <div className="flex flex-col pt-3.5">
-//       <img
-//         loading="lazy"
-//         src={image}
-//         alt={name}
-//         className="object-contain z-10 self-center mt-3.5 max-w-full aspect-square w-[101px]"
-//       />
-//       <div
-//         className={`flex flex-col items-center px-7 pt-32 pb-5 mt-0 ${bgColor} rounded min-h-[194px] max-md:px-5 max-md:pt-24`}
-//       >
-//         <div className="flex flex-col items-center">
-//           <div className="text-lg font-semibold text-slate-700">{name}</div>
-//           <div className="mt-1.5 text-sm font-medium text-zinc-400">
-//             {items} Items
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default CategoryCard;
